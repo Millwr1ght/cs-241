108342cc80dad6c624a17a5f10d07591b0487fe7
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-06-02 08:43:06 -0500

    Update check07b.py

--- check07/check07b.py
+++ check07/check07b.py
@@ -5,51 +5,80 @@ Author: Br. Burton
 Demonstrates abstract base classes.
 """
 
-#TODO: Import anything you need for Abstract Base Classes / methods
+from abc import ABC, abstractmethod
+# from math import pi # but nooooo, this is wroooong
+pi = 3.14
 
 
-#TODO: convert this to an ABC 
-class Shape():
+class Shape(ABC):
     def __init__(self):
         self.name = ""
-    
+
     def display(self):
-        print("{} - {:.2f}".format(self.name, self.get_area()))
+        print(f"{self.name} - {self.get_area():.2f}")
 
-    #TODO: Add an abstractmethod here called get_area
+    # TODO: Add an abstractmethod here called get_area
+    @abstractmethod
+    def get_area(self):
+        """ """
+        raise ConnectionError('overriden method necessary')
 
 
-#TODO: Create a Circle class here that derives from Shape
+class Circle(Shape):
+    """ """
+
+    def __init__(self, Radius: float = 0.0):
+        """ variable setup"""
+        super().__init__()
+
+        self.name = 'Circle'
+        self.radius = Radius
+
+    def get_area(self):
+        """ return 3.14 * radius * radius """
+        return pi * (self.radius ** 2)
 
 
-#TODO: Create a Rectangle class here that derives from Shape
+class Rectangle(Shape):
+    """ """
+
+    def __init__(self, Length: float = 0.0, Width: float = 0.0):
+        """ variable setup"""
+        super().__init__()
+
+        self.name = 'Rectangle'
+        self.length = Length
+        self.width = Width
+
+    def get_area(self):
+        """ return lxw"""
+        return self.width * self.length
+
 
 def main():
 
-    #TODO: Declare your list of shapes here
-
+    shapes = []
     command = ""
 
     while command != "q":
-        command = input("Please enter 'c' for circle, 'r' for rectangle or 'q' to quit: ")
+        command = input(
+            "Please enter 'c' for circle, 'r' for rectangle or 'q' to quit: ")
 
         if command == "c":
             radius = float(input("Enter the radius: "))
-            #TODO: Declare your Circle here, set its radius, and
-            # add it to the list
-        
+            c = Circle(radius)
+            shapes.append(c)
+
         elif command == "r":
             length = float(input("Enter the length: "))
             width = float(input("Enter the width: "))
-            #TODO: Declare your Rectangle here, set its length
-            # and width, and add it to the list
-
+            r = Rectangle(length, width)
+            shapes.append(r)
 
     # Done entering shapes, now lets print them all out:
-
-    #TODO: Loop through each shape in the list, and call its display function
+    for shape in shapes:
+        shape.display()
 
 
 if __name__ == "__main__":
     main()
-
