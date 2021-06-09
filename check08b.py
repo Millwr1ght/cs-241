bfd2dabe9d53e5a5a0ab74c27211cb6c19263a15
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-06-09 15:05:25 -0500

    check08

--- check08b.py
+++ check08b.py
@@ -1,5 +1,5 @@
 """ 
-File: check08a.py
+File: check08b.py
 Author: Nathan Johnston
 """
 
@@ -19,13 +19,13 @@ class GPA:
 
     def __init__(self):
         """"""
-        self._gpa = 0.0
+        self.gpa = 0.0
 
-    def _get_gpa(self):
+    def get_gpa(self):
         """ get the GPA value """
-        return self._gpa
+        return self.gpa
 
-    def _set_gpa(self, value):
+    def set_gpa(self, value):
         """ set the GPA to a value between 0 and 4 
         If a GPA less that 0 is entered, 
             this should instead set it to 0. 
@@ -33,70 +33,66 @@ class GPA:
             this should instead set the value to 4.0.
         """
         if value < 0:
-            self._gpa = 0
+            self.gpa = 0
         elif value > 4.0:
-            self._gpa = 4.0
+            self.gpa = 4.0
         else:
-            self._gpa = value
+            self.gpa = value
 
-    @property
-    def letter(self):
+    def get_letter(self):
         """ get the associated letter value 
         'determine the correct letter for the stored gpa, and return it'
         """
-        if self._gpa < 1.0:
+        if self.gpa < 1.0:
             return 'F'
-        elif self._gpa >= 1.0 and self._gpa < 2.0:
+        elif self.gpa >= 1.0 and self.gpa < 2.0:
             return 'D'
-        elif self._gpa >= 2.0 and self._gpa < 3.0:
+        elif self.gpa >= 2.0 and self.gpa < 3.0:
             return 'C'
-        elif self._gpa >= 3.0 and self._gpa < 4.0:
+        elif self.gpa >= 3.0 and self.gpa < 4.0:
             return 'B'
         else:
             return 'A'
 
-    @letter.setter
-    def letter(self, letter):
+    def set_letter(self, letter):
         """ set the associated letter value for the GPA 
         'determine the appropriate gpa value and store that.'
         { F: 0.0; D: 1.0; C: 2.0; B: 3.0; A: 4.0 }
         """
         if letter.upper() == 'F':
-            self._gpa = 0.0
+            self.gpa = 0.0
         elif letter.upper() == 'D':
-            self._gpa = 1.0
+            self.gpa = 1.0
         elif letter.upper() == 'C':
-            self._gpa = 2.0
+            self.gpa = 2.0
         elif letter.upper() == 'B':
-            self._gpa = 3.0
+            self.gpa = 3.0
         elif letter.upper() == 'A':
-            self._gpa = 4.0
-
-    gpa = property(_get_gpa, _set_gpa)
+            self.gpa = 4.0
 
 
 def main():
     student = GPA()
 
     print("Initial values:")
-    print("GPA: {:.2f}".format(student.gpa))
-    print("Letter: {}".format(student.letter))
+    print("GPA: {:.2f}".format(student.get_gpa()))
+    print("Letter: {}".format(student.get_letter()))
 
     value = float(input("Enter a new GPA: "))
 
-    student.gpa = value
+    student.set_gpa(value)
 
     print("After setting value:")
-    print("GPA: {:.2f}".format(student.gpa))
-    print("Letter: {}".format(student.letter))
+    print("GPA: {:.2f}".format(student.get_gpa()))
+    print("Letter: {}".format(student.get_letter()))
 
     letter = input("Enter a new letter: ")
 
-    student.letter = letter
+    student.set_letter(letter)
 
     print("After setting letter:")
-    print("GPA: {:.2f}".format(student.gpa))
-    print("Letter: {}".format(student.letter))
+    print("GPA: {:.2f}".format(student.get_gpa()))
+    print("Letter: {}".format(student.get_letter()))
 
 
 if __name__ == "__main__":
